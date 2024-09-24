0192a6880bc2db939c4ffe70e4d8622442b98832
H.N. Gibson <revhngibson@gmail.com>
2024-09-24 15:44:37 -0500

    Finished Assignment

--- python_modules_and_data_handling/main.py
+++ python_modules_and_data_handling/main.py
@@ -3,7 +3,23 @@
 # Task 1: Your Mood Today - Problem Statement: Create a Python program using a custom module that asks the user how they are feeling today and responds with a custom message based on the mood entered. - Code Example:
 
 import mood_responses
-mood = input("How are you feeling today? ")
-print(mood_responses.mood_response(mood))
+
+class UserInputEmpty(Exception):
+    pass
+
+while True:
+    try:
+        mood = input("How are you feeling today? (type 'q' to quit)\n").lower
+        if mood() == "q":
+            break
+        elif mood() == "":
+            raise UserInputEmpty
+        else:
+            print(mood_responses.mood_response(mood()))
+    except UserInputEmpty:
+        print("Input was empty. Please try again.")
+    finally:
+        print("Don't have a good day. Have a great day!")
+print ("Goodbye!")
 
 # - Expected Outcome: The program should be able to take a user's mood as input (e.g., happy, sad, excited) and return a corresponding custom message.
--- python_modules_and_data_handling/mood_responses.py
+++ python_modules_and_data_handling/mood_responses.py
@@ -1,2 +1,11 @@
 def mood_response(mood):
-    pass
+    if mood == "happy":
+        return "Today is a good day!"
+    elif mood == "sad":
+        return "I'm sorry to hear that! Have you considered going for a walk or reaching out to a friend?"
+    elif mood == "excited":
+        return "Woot! I'm hyped for you!"
+    elif mood == "angry":
+        return "We all get mad from time to time. Take a deep breath and count to four."
+    else:
+        return "Sorry, I'm a computer and my programmer did not think to include that mood. Blame them or feel free to share a different word for your mood?"
